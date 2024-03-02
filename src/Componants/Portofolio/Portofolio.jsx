2e8f43d1e7faecef3ba32fbee6ee5e96f81ba68e
AhmedAbdElbasset <ahmedalmajek393@gmail.com>
2024-03-02 02:52:00 +0200

    fix error

--- src/Componants/Portofolio/Portofolio.jsx
+++ src/Componants/Portofolio/Portofolio.jsx
@@ -15,6 +15,7 @@ const repos=[
         reoLink:'https://github.com/AhmedAbdElbasset/KASPER-TEMPLET'
     },
     {
+        key:'1',
         img:Elzero,
         title:'elzero',
         dis:'elzero portofolio',
@@ -23,6 +24,7 @@ const repos=[
 
     },
     {
+        key:'2',
         img:Dashbord,
         title:'dashbord',
         dis:'elzero dashbord',
@@ -31,6 +33,7 @@ const repos=[
 
     },
     {
+        key:'3',
         img:Moviex,
         title:'moviex',
         dis:'movies store',
@@ -38,6 +41,7 @@ const repos=[
         reoLink:'https://github.com/AhmedAbdElbasset/Moviex'
     },
     {
+        key:'4',
         img:HardRock,
         title:'hard rock',
         dis:'e commerce',
@@ -52,7 +56,7 @@ function Portofollio(){
             <p className="main-header">Portfolio</p>
             <div className="work">
                 {repos.map(repo=>(
-                    <div className='box' key={repo.title}>
+                    <div className='box' key={repo.key}>
                         <img src={repo.img} alt="" />
                         <div className='layer'>
                             <p>{repo.title}</p>
