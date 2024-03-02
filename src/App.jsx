77de44c3412cff028042e4b858c47f4009454110
AhmedAbdElbasset <ahmedalmajek393@gmail.com>
2024-03-02 03:32:57 +0200

    fuck

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import Landing from "./Componants/Landing/Landing.jsx";
 import About from "./Componants/about/About.jsx";
 import Skills from "./Componants/Skills/Skills.jsx";
-import Portofollio from "./Componants/Portofolio/Portofolio.jsx";
+import Portofollio from "./Componants/Portofolio/Portofolio";
 import "./index.css";
 import {motion, AnimatePresence } from "framer-motion";
 import Contact from "./Componants/Contact/Contact";
 function App() {
-  const [load, setLoad] = useState(false);
+  const [load, setLoad] = useState(true);
   window.onload = function () {
     setTimeout(()=>{
-      // setLoad(prevState=>!prevState)
-      setLoad(false)
+      setLoad(prevState=>!prevState)
     },2000)
   };
   return (
