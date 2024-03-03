dbc672eb0c5316151d27fa2f5ed05c121d610080
AhmedAbdElbasset <ahmedalmajek393@gmail.com>
2024-03-03 20:44:06 +0200

    eix error

--- src/Componants/Header/Header.jsx
+++ src/Componants/Header/Header.jsx
@@ -5,11 +5,11 @@ import { HashLink as NavLink } from "react-router-hash-link";
 const Header = () => {
   return (
     <div className="Head">
-      <a  href ={'\#landing'} />
-        <a  href={"\#about"}/>
-        <a  href={"\#Skills"}/>
-        <a  href={"\#Portfolio"}/>
-        <a  href={"\#Contact"}/>
+      <NavLink  to ={'#landing'} end='true'/>
+        <NavLink  to={"#about"}/>
+        <NavLink  to={"#Skills"}/>
+        <NavLink  to={"#Portfolio"}/>
+        <NavLink  to={"#Contact"}/>
     </div>
   );
 };
