26f1d44aa3a4cc3456becf2dc2b19ddb3a526160
AhmedAbdElbasset <ahmedalmajek393@gmail.com>
2024-03-03 20:26:51 +0200

    fix errors

--- src/Componants/Header/Header.jsx
+++ src/Componants/Header/Header.jsx
@@ -5,11 +5,11 @@ import { HashLink as NavLink } from "react-router-hash-link";
 const Header = () => {
   return (
     <div className="Head">
-      <a  href ={'/#landing'} />
-        <a  href={"/#about"}/>
-        <a  href={"/#Skills"}/>
-        <a  href={"/#Portfolio"}/>
-        <a  href={"/#Contact"}/>
+      <a  href ={'\#landing'} />
+        <a  href={"\#about"}/>
+        <a  href={"\#Skills"}/>
+        <a  href={"\#Portfolio"}/>
+        <a  href={"\#Contact"}/>
     </div>
   );
 };
